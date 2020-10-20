158f4dbee1c5fc149f1287fa3b8c648c0e77f496
Ioannis Valasakis <code@wizofe.uk>
2020-10-20 21:51:00 +0100

    Fix PEP8: use def when binds lambda to a name

--- deepinpy/recons/dbp/dbp.py
+++ deepinpy/recons/dbp/dbp.py
@@ -4,38 +4,57 @@ import numpy as np
 import torch
 import tqdm
 
-from deepinpy.utils import utils
-from  deepinpy.opt import ConjGrad
 from deepinpy import opt
 from deepinpy.forwards import MultiChannelMRI
-from deepinpy.models import ResNet5Block, ResNet
+from deepinpy.models import ResNet, ResNet5Block
+from deepinpy.opt import ConjGrad
 from deepinpy.recons import Recon
+from deepinpy.utils import utils
+
 
 class DeepBasisPursuitRecon(Recon):
-
     def __init__(self, hparams):
         super(DeepBasisPursuitRecon, self).__init__(hparams)
         self.l2lam = torch.nn.Parameter(torch.tensor(hparams.l2lam_init))
         self.num_admm = hparams.num_admm
 
-        if hparams.network == 'ResNet5Block':
-            self.denoiser = ResNet5Block(num_filters=hparams.latent_channels, filter_size=7, batch_norm=hparams.batch_norm)
-        elif hparams.network == 'ResNet':
-            self.denoiser = ResNet(latent_channels=hparams.latent_channels, num_blocks=hparams.num_blocks, kernel_size=7, batch_norm=hparams.batch_norm)
+        if hparams.network == "ResNet5Block":
+            self.denoiser = ResNet5Block(
+                num_filters=hparams.latent_channels,
+                filter_size=7,
+                batch_norm=hparams.batch_norm,
+            )
+        elif hparams.network == "ResNet":
+            self.denoiser = ResNet(
+                latent_channels=hparams.latent_channels,
+                num_blocks=hparams.num_blocks,
+                kernel_size=7,
+                batch_norm=hparams.batch_norm,
+            )
 
         self.debug_level = 0
 
     def batch(self, data):
 
-        maps = data['maps']
-        masks = data['masks']
-        inp = data['out']
+        maps = data["maps"]
+        masks = data["masks"]
+        inp = data["out"]
 
-        self.A = MultiChannelMRI(maps, masks, l2lam=0., img_shape=data['imgs'].shape, use_sigpy=self.hparams.use_sigpy, noncart=self.hparams.noncart)
+        self.A = MultiChannelMRI(
+            maps,
+            masks,
+            l2lam=0.0,
+            img_shape=data["imgs"].shape,
+            use_sigpy=self.hparams.use_sigpy,
+            noncart=self.hparams.noncart,
+        )
         self.x_adj = self.A.adjoint(inp)
 
     def forward(self, y):
-        eps = opt.ip_batch(self.A.maps.shape[1] * self.A.mask.sum((1, 2))).sqrt() * self.hparams.stdev
+        eps = (
+            opt.ip_batch(self.A.maps.shape[1] * self.A.mask.sum((1, 2))).sqrt()
+            * self.hparams.stdev
+        )
         x = self.A.adjoint(y)
         z = self.A(x)
         z_old = z
@@ -46,7 +65,12 @@ class DeepBasisPursuitRecon(Recon):
         z_old.requires_grad = False
         u.requires_grad = False
 
-        self.num_cg = np.zeros((self.hparams.num_unrolls,self.hparams.num_admm,))
+        self.num_cg = np.zeros(
+            (
+                self.hparams.num_unrolls,
+                self.hparams.num_admm,
+            )
+        )
 
         for i in range(self.hparams.num_unrolls):
             r = self.denoiser(x)
@@ -54,8 +78,14 @@ class DeepBasisPursuitRecon(Recon):
             for j in range(self.hparams.num_admm):
 
                 rhs = self.l2lam * self.A.adjoint(z - u) + r
-                fun = lambda xx: self.l2lam * self.A.normal(xx) + xx
-                cg_op = ConjGrad(rhs, fun, max_iter=self.hparams.cg_max_iter, eps=self.hparams.cg_eps, verbose=False)
+                def fun(xx) return self.l2lam * self.A.normal(xx) + xx
+                cg_op = ConjGrad(
+                    rhs,
+                    fun,
+                    max_iter=self.hparams.cg_max_iter,
+                    eps=self.hparams.cg_eps,
+                    verbose=False,
+                )
                 x = cg_op.forward(x)
                 n_cg = cg_op.num_cg
                 self.num_cg[i, j] = n_cg
@@ -67,15 +97,15 @@ class DeepBasisPursuitRecon(Recon):
 
                 # check ADMM convergence
                 Ax = self.A(x)
-                r_norm = opt.ip_batch(Ax-z).sqrt()
+                r_norm = opt.ip_batch(Ax - z).sqrt()
                 s_norm = opt.ip_batch(self.l2lam * self.A.adjoint(z - z_old)).sqrt()
-                if (r_norm + s_norm).max() < 1E-2:
-                    if self.debug_level > 0:
-                        tqdm.tqdm.write('stopping early, a={}'.format(a))
+                if (r_norm + s_norm).max() < 1e-2:
+                    if self.debug_level > 0:    # where is a defined?
+                        tqdm.tqdm.write("stopping early, a={}".format(a))
                     break
         return x
 
     def get_metadata(self):
         return {
-                'num_cg': self.num_cg.ravel(),
-                }
+            "num_cg": self.num_cg.ravel(),
+        }
